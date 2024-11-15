fc9493aa5d92dcad0c3b353e83d4da69ee1730e1
srijan <srijanchowdary@gmail.com>
2024-11-15 19:54:30 +0000

    2

--- staff_list/models.py
+++ staff_list/models.py
@@ -8,9 +8,11 @@ class Room(models.Model):
         return f"{self.building} - {self.number}"
 
 
+from django.db import models
+
 class Faculty(models.Model):
-    name = models.CharField(max_length=64)
-    dean = models.CharField(max_length=64, null=True, blank=True)
+    name = models.CharField(max_length=64, unique=True)  # Add unique constraint
+    dean = models.CharField(max_length=64, null=True)
 
     def __str__(self):
         return self.name
