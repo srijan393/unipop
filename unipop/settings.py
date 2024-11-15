93a8a15baba36241698bc8434ad10079dc56313c
srijan <srijanchowdary@gmail.com>
2024-11-15 19:48:45 +0000

    2

--- unipop/settings.py
+++ unipop/settings.py
@@ -37,7 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'staff_list.apps.StaffListConfig',
 ]
+
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
